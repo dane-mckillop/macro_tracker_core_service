c63a65a98feb04c96ab9aa8cda6666f78a55529d
Dane <dane.mckillop@gmail.com>
2025-02-25 00:25:30 +1000

    initial commit

---
+++ Models/FoodContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FoodApi.Models
+{
+    public class FoodPer100gContext : DbContext
+    {
+        public FoodPer100gContext(DbContextOptions<FoodPer100gContext> options) : base(options) { }
+
+        public DbSet<FoodPer100g> FoodsPer100g { get; set; }
+    }
+}
---
+++ Models/FoodPer100g.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodApi.Models
+{
+    public class FoodPer100g
+    {
+        public int FoodId { get; set; }
+        public string Name { get; set; }
+        public float Protein { get; set; }
+        public float Calories { get; set; }
+        public float Carbs { get; set; }
+        public float Fats { get; set; }
+        public float Price { get; set; }
+    }
+}
